c37678482f6020aa1c4d1d8c238ea80ec4aa0ab6
idiallo <idiallo@hotmail.com>
2025-06-05 12:42:16 +0000

    chap2 donegit add .

--- ch02/rectangle-square/src/main.rs
+++ ch02/rectangle-square/src/main.rs
@@ -1,3 +1,5 @@
+use std::any::Any;
+
 struct Rectangle {
     width: i32,
     height: i32,
@@ -9,6 +11,7 @@ impl Rectangle {
     }
 }
 
+#[derive(Debug)]
 struct Square {
     length: i32,
 }
@@ -26,6 +29,8 @@ pub trait Rectangular {
     fn get_width(&self) -> i32;
     fn get_height(&self) -> i32;
     fn get_area(&self) -> i32;
+
+    fn DOWNCAST(&self) -> &dyn Any;
 }
 
 impl Rectangular for Rectangle {
@@ -38,6 +43,12 @@ impl Rectangular for Rectangle {
     fn get_area(&self) -> i32 {
         self.width * self.height
     }
+
+    fn DOWNCAST(&self) -> &dyn Any {
+        self
+    }
+
+    
 }
 
 impl Rectangular for Square {
@@ -50,11 +61,33 @@ impl Rectangular for Square {
     fn get_area(&self) -> i32 {
         self.length * self.length
     }
+
+    fn DOWNCAST(&self) -> &dyn Any {
+        self
+    }
 }
 
 fn main() {
     let rect = Rectangle::new(2, 3);
     let square = Square::new(5);
+
+
+    let myVect: Vec<Box<dyn Rectangular>> = vec![Box::new(Square::new(10)),Box::new(Rectangle::new(10,30))];
+
+
+    let values = myVect.iter().map(|x| {
+
+        println!("height: {} width:{} ",x.get_height(),x.get_width());
+
+        if let Some(v) = x.DOWNCAST().downcast_ref::<Square>() {
+            println!("IT'S A SQUUAREEEEEE DOWNCASTE {v:?}");
+        }else {
+            println!("IS NOT A SQUARE!!!!!")
+        }
+
+    }).collect::<Vec<()>>();
+
+    //println!("{values:?}");
 
     println!(
         "rect has width {}, height {}, and area {}",
